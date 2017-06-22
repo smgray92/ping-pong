ea45cb95b414a59b5d19c328795760a53490d911
Sharon Gray <smgray113@gmail.com>
2017-06-22 01:02:50 -0700

    Make changes to jQuery add for loop functions.

--- js/scripts.js
+++ js/scripts.js
@@ -3,26 +3,38 @@ var num = function(number) {
 
   for (var i = 1; i <= number; i++) {
     arrayNumber.push(i);
-    }
+  }
   return arrayNumber;
+
+  var arrayList = [];
+  arrayList.push(arrayNumber);
+  for (var i = 1; i <= number; i++) {
+    $("#list").append("<li>"+i+"</li>");
+  }
+  return arrayList;
 }
-
-
 
 
 
 
 $(document).ready(function() {
   $("form#enterNumber").submit(function(event) {
-  event.preventDefault();
-  // var arrayNumber = [];
-  // arrayNumber.push();
-  // arrayNumber.forEach(function(item){
-  //      $(".item1").append("<li>"+item+"</li>");
-  //    });
+    event.preventDefault();
+    var arrayList =  [];
+    // var arrayNumber = [];
+    // arrayNumber.push();
+    // arrayNumber.forEach(function(item){
+    //      $(".item1").append("<li>"+item+"</li>");
+    //    });
     var number = $("input#number").val();
     var result = num(number);
     console.log(result);
-    $("#list").text(result);
+    arrayList.push(result);
+
+    arrayList.forEach(function(list) {
+      $(".list").append("<li>"+list+"</li>");
+    });
+    // $("#output").text(result);
+    console.log(arrayList);
   });
 });
