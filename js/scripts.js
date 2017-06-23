3b424130156456c99e218df92d3de1c2a43cb7dc
Sharon Gray <smgray113@gmail.com>
2017-06-22 20:33:09 -0700

    Edit scripts.js to allow a list to come out as an output.

--- js/scripts.js
+++ js/scripts.js
@@ -16,39 +16,28 @@ var num = function(number) {
     }
   }
   return arrayNumber;
-
-  // var arrayLists = [];
-  // arrayLists.push(arrayNumber);
-
-  // for (var i = 1; i <= number; i++) {
-  //   $(".list").append("<li>"+i+"</li>");
-  // }
-  // return arrayLists;
 }
+
 
 $(document).ready(function() {
   $("form#enterNumber").submit(function(event) {
 
     event.preventDefault();
-$("#output").hide();
-    var arrayLists = [];
-    // var arrayNumber = [];
-    // arrayNumber.push();
-    // arrayNumber.forEach(function(item){
-    //      $(".item1").append("<li>"+item+"</li>");
-    //    });
+    $("#output").hide();
+
+
     var number = $("input#number").val();
+
+
     var result = num(number);
     console.log(result);
-    arrayLists.push(result);
 
-
-
-    arrayLists.forEach(function(item) {
+    
+    // $(".list").remove();
+    result.forEach(function(item) {
       $(".list").append("<li>"+item+"</li>");
     });
-    console.log(arrayLists);
-    $("#output").show(result);
 
+    $("#output").show(result);
   });
 });
